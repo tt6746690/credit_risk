0a70fd69e37f5911064466ab89ed34dc6b0e64a9
Mark <peiqi1122@gmail.com>
2018-10-24 23:34:30 -0400

    run parallel

--- scripts/run_multiple.py
+++ scripts/run_multiple.py
@@ -2,6 +2,10 @@ import os
 import time
 import subprocess as sp
 import shlex
+import os
+
+julia = "{}/julia-0.7.0/bin/julia".format(os.environ['HOME'])
+# julia = "/Applications/Julia-0.7.app/Contents/Resources/julia/bin/julia"
 
 
 n = 2500
@@ -9,7 +13,6 @@ c = 4
 s = 5
 nz = 9223372036854775807
 ne = 10000
-
 reps = 3
 
 cmds = []
@@ -19,13 +22,15 @@ for l in [0.1, 0.2, 0.3, 0.4]:
         ("glassermanli", 5),
     ]:
         for rep in range(reps):
+
             filename = "{}_ninit_{}_n_{}_c_{}_s_{}_l_{}_ne_{}_rep_{}".format(
                 algo, n_init, n, c, s, l, ne, rep
             )
 
             cmd = """
-    julia7 src/main.jl --n {n} --c {c} --s {s} --l {l} --nz {nz} --ne {ne} --filename {filename} --n_init {n_init} --a {algo}
+            {julia} src/main.jl --n {n} --c {c} --s {s} --l {l} --nz {nz} --ne {ne} --filename {filename} --n_init {n_init} --a {algo}
             """.format(
+                julia=julia,
                 n=n,
                 c=c,
                 s=s,
@@ -40,7 +45,7 @@ for l in [0.1, 0.2, 0.3, 0.4]:
             cmds.append(cmd)
 
 
-show = True
+show = False
 if show:
     for cmd in cmds:
         print(cmd)
@@ -48,7 +53,7 @@ else:
     procs = []
     for cmd in cmds:
         print(cmd)
-        proc = sp.Popen(cmd, shell=True)
+        proc = sp.Popen(cmd, shell=True, env=os.environ)
         procs.append(proc)
 
     print("starting to wait for subprocesses...")
